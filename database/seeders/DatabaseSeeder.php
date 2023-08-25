b66cd909f5ebae9b40ca0c314107737afd43c2cc
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-08-25 16:06:02 +0700

    feat: faker for categories & tags

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Tag;
 use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,11 +22,30 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $isGenerateUid = false;
+        while (!$isGenerateUid) {
+            $uid = Str::random(10);
+            $isExist = User::where('uid', $uid)->first();
+            if (!$isExist) {
+                $isGenerateUid = true;
+            }
+        }
         User::create([
+            'uid' => $uid,
             'nama' => 'Nda',
             'email' => 'nda@gmail.com',
             'password' => bcrypt('nda12345'),
             'role' => 2
         ]);
+
+        $faker = \Faker\Factory::create('id_ID');
+        for ($i = 0; $i < 10; $i++) {
+            Category::create([
+                'nama_kategori' => ucwords($faker->unique()->word()),
+            ]);
+            Tag::create([
+                'nama_tag' => ucwords($faker->unique()->word()),
+            ]);
+        }
     }
 }
